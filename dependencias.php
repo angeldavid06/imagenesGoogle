810c04a4f5f666921b10a6a6318edeb6a63feeb8
angeldavid06 <mendozaangel.3243@gmail.com>
2020-10-01 17:44:35 -0500

    primir commit

---
+++ dependencias.php
@@ -0,0 +1,10 @@
+<meta charset="UFT-8">
+<link rel="stylesheet" href="librerias/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="librerias/gridder/css/jquery.gridder.min.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="css/demo.css">
+<link rel="stylesheet" href="css/estilos.css">
+
+<script src="librerias/jquery-3.3.1.min.js"></script>
+<script src="librerias/bootstrap/js/bootstrap.js"></script>
+<script src="librerias/gridder/js/jquery.gridder.min.js"></script>
